2096cf2a662a9839b8af2cee58467f85d8bd525c
maxmaster55 <youssefalkashef000@gmail.com>
2023-01-23 23:46:07 +0200

    go back button is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-import { Route, Routes } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Navbar from './components/Navbar'
 import HomePage from './pages/HomePage'
 import WatchMoviePage from './pages/WatchMoviePage'
 
 function App() {
+  const location = useLocation()
+  const [isHomePage, setIsHomePage] = useState(true)
+
+  useEffect(() => {
+    if (location.pathname === "/") {
+      console.log("is home page")
+      setIsHomePage(true)
+    } else {
+      console.log("is not home page")
+      setIsHomePage(false)
+    }
+
+  }, [location])
 
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/watch-movie/:movieId" element={<WatchMoviePage />} />
-    </Routes>
+    <>
+      <Navbar isHomePage={isHomePage} />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/watch-movie/:movieId" element={<WatchMoviePage />} />
+      </Routes>
+    </>
   )
 }
 
--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -6,7 +6,6 @@ function MainGrid({ children }) {
 
         <div className="container mx-auto">
             <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
-
                 {children}
             </div>
         </div>
