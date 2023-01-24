892d331cc8508b56759be984d1b0b00c1a0751ca
maxmaster55 <youssefalkashef000@gmail.com>
2023-01-24 15:26:40 +0200

    refactoring the button into a custom component

---
+++ src/components/CustomButton.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+
+
+export default function CustomButton({ handleClick, children }) {
+
+    return (
+
+        <motion.div
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            onClick={() => handleClick()}
+            className="cursor-pointer text-white font-mono text-xl flex items-center gap-1
+                hover:text-yellow-500 bg-sky-900 bg-opacity-50 hover:bg-opacity-100
+                hover:border-yellow-500 ml-2 p-2 rounded-full"
+        >
+            {children}
+        </motion.div>
+    )
+}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { FaSearch } from 'react-icons/fa';
 import { FaArrowLeft } from 'react-icons/fa';
+import { MdCancel } from 'react-icons/md';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
+import CustomButton from './CustomButton';
 
-export default function Navbar({ isHomePage, isSearchActive, setIsSearchActive }) {
+export default function Navbar({ isHomePage, isSearchActive, setIsSearchActive, searchValue, setSearchValue }) {
 
     return (
         <div className="bg-sky-600 p-2 shadow-lg">
@@ -17,7 +19,12 @@ export default function Navbar({ isHomePage, isSearchActive, setIsSearchActive }
                         Angry Watch
                     </div>
                     <div className="flex self-center">
-                        <SearchButton setIsActive={(val) => setIsSearchActive(val)} isActive={isSearchActive} />
+                        <SearchButton
+                            setIsActive={(val) => setIsSearchActive(val)}
+                            isActive={isSearchActive}
+                            searchValue={searchValue}
+                            setSearchValue={(e) => setSearchValue(e.target.value)}
+                        />
                     </div>
                 </div>
             </div>
@@ -29,50 +36,68 @@ export default function Navbar({ isHomePage, isSearchActive, setIsSearchActive }
 
 function GoBackButton({ to }) {
     return (
-        <motion.div
-            whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.9 }}
-        >
-            <Link to={to}>
-                <button className="text-white font-mono text-xl flex items-center gap-1
-            hover:text-yellow-500 bg-sky-900 bg-opacity-50 hover:bg-opacity-100
-            hover:border-yellow-500 ml-2 p-2 rounded-full">
-                    <FaArrowLeft />
-                </button>
-            </Link>
-        </motion.div>
+
+        <Link to={to}>
+            <CustomButton handleClick={() => { }}>
+                <FaArrowLeft />
+            </CustomButton>
+        </Link>
+
     )
 }
 
 function SearchButton({ isActive, setIsActive, searchValue, setSearchValue }) {
+
+    function handleSearchButton() {
+        if (isActive) {
+            if (searchValue == "") {
+                setIsActive(false)
+            } else {
+                console.log("searching")
+            }
+        } else {
+            setIsActive(!isActive)
+        }
+    }
+    useEffect(() => {
+        // close search bar if search value is empty
+        if (isActive && searchValue !== "") {
+            console.log("searching")
+        }
+        // handle escape key press
+        function handleEscapeKeyPress(e) {
+            if (e.key === "Escape") {
+                setIsActive(false)
+            }
+        }
+        window.addEventListener("keydown", handleEscapeKeyPress)
+        return () => {
+            window.removeEventListener("keydown", handleEscapeKeyPress)
+        }
+
+    }, [searchValue, isActive])
+
     return (
         <div className='flex'>
             <motion.input
                 initial={{ opacity: 0, x: 100 }}
                 animate={{
                     opacity: 1,
-                    visibility: isActive ? "visible" : "hidden",
                     x: isActive ? 0 : 10
                 }}
                 transition={{ type: "spring", stiffness: 300, damping: 15 }}
-                className="bg-sky-900 bg-opacity-50 hover:bg-opacity-100
-                hover:border-yellow-500 px-4 rounded-full text-white"
+                className={(!isActive ? "hidden" : "inline") + " bg-sky-900 bg-opacity-50 hover:bg-opacity-100\
+                hover:border-yellow-500 px-4 rounded-full text-white relative "}
                 type="text"
                 value={searchValue}
-                onChange={(e) => { setSearchValue(e.target.value) }}
-                placeholder='Search'
+                onChange={(e) => { setSearchValue(e) }}
+                placeholder='Search for a movie...'
+                onKeyPress={(e) => { }}
             >
             </motion.input>
-
-            <div
-                onClick={() => setIsActive(!isActive)}
-                className="text-white font-mono text-xl flex items-center gap-1
-                hover:text-yellow-500 bg-sky-900 bg-opacity-50 hover:bg-opacity-100
-                hover:border-yellow-500 ml-2 p-2 rounded-full"
-            >
-                <FaSearch />
-            </div>
-
+            <CustomButton handleClick={() => handleSearchButton()}>
+                {(isActive && searchValue === "") ? <MdCancel /> : <FaSearch />}
+            </CustomButton>
         </div>
     )
 }
