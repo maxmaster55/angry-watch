ddfdc1e8296c9e849e40408ab8d5a6de295d4cb5
maxmaster55 <youssefalkashef000@gmail.com>
2023-01-27 23:35:21 +0200

    added a test spider to make testing easier

--- src/Backend/spiders/TestSpider.ts
+++ src/Backend/spiders/TestSpider.ts
@@ -4,14 +4,55 @@ import Movie from '@myTypes/Movie';
 
 export default class TestSpider implements Spider {
     BaseUrl: string;
-    getMovie(name: string): Promise<Movie> {
-        throw new Error('Method not implemented.');
+    TestMovies: Movie[] = [
+        {
+            title_english: 'Test Movie 1',
+            year: 2020,
+            imdb_code: 'tt1234567',
+            slug: "test-movie-1-2020",
+            small_cover_image: 'https://m.media-amazon.com/images/M/MV5BMjMwNDkxMTgzOF5BMl5BanBnXkFtZTgwNTkwNTQ3NjM@._V1_FMjpg_UX1000_.jpg',
+        },
+        {
+            title_english: 'Test Movie 2',
+            year: 2020,
+            imdb_code: 'tt1234567',
+            slug: "test-movie-2-2020",
+            small_cover_image: 'https://m.media-amazon.com/images/M/MV5BMjMwNDkxMTgzOF5BMl5BanBnXkFtZTgwNTkwNTQ3NjM@._V1_FMjpg_UX1000_.jpg',
+        },
+        {
+            title_english: 'Test Movie 3',
+            year: 2020,
+            imdb_code: 'tt1234567',
+            slug: "test-movie-3-2020",
+            small_cover_image: 'https://m.media-amazon.com/images/M/MV5BMjMwNDkxMTgzOF5BMl5BanBnXkFtZTgwNTkwNTQ3NjM@._V1_FMjpg_UX1000_.jpg',
+        },
+    ]
+
+
+
+    async getMovie(name: string): Promise<Movie> {
+        const w = await this.wait(2)
+        const movie = this.TestMovies.find(movie => movie.slug === name)
+        return Promise.resolve(movie)
+
     }
     makeRequest(url: string): Promise<string> {
         throw new Error('Method not implemented.');
     }
-    search(query: string): Promise<Movie[]> {
-        throw new Error('Method not implemented.');
+    async search(query: string): Promise<Movie[]> {
+        const w = await this.wait(2)
+        const movies = this.TestMovies.filter(movie => movie.title_english.includes(query))
+        return Promise.resolve(movies)
     }
+
+    async wait(seconds: number): Promise<void> {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve();
+            }, seconds * 1000);
+        });
+    }
+
+
 
 }
