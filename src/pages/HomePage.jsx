1818d945f8a65aa1ec7fdd05ecc249bb0ba92c94
maxmaster55 <youssefalkashef000@gmail.com>
2023-01-29 18:04:19 +0200

    did some cleaning

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,21 +1,11 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import SearchButton from '../components/Buttons/SearchButton';
 import Centered from '../components/utils/Centered';
 
 //FIXME: fix the design of the search button
-function HomePage({ isHomePage, isSearchActive, setIsSearchActive, searchValue, setSearchValue }) {
+function HomePage({ isSearchActive, setIsSearchActive, searchValue, setSearchValue }) {
 
     // TODO: do something with this function
-    const navigate = useNavigate();
-    function handleCardClick(type, id) {
-        if (type === "Movie") {
-            navigate(`/watch-movie/${id}`);
-        } else if (type === "TV Show") {
-            navigate(`/watch-show/${id}`);
-        }
-    }
-
     return (
         <Centered>
             <SearchButton
